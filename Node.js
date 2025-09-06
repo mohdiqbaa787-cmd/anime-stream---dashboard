00d85a5ea9b54bdb610968e7fffbf90e17071a0a
mohdiqbaa787-cmd <mohdiqbaa.787@gmail.com>
2025-09-06 19:42:34 +0530

    Create Node.js

---
+++ Node.js
@@ -0,0 +1,12 @@
+{
+  "name": "anime-site",
+  "version": "1.0.0",
+  "scripts": {
+    "build": "node scripts/convert.js"
+  },
+  "dependencies": {
+    "gray-matter": "^4.0.3"
+  }
+"scripts": {
+  "build": "node scripts/convert.js"
+}
